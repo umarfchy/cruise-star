21fbe0bbf5c68a80b63c16f7cb67abf9ff5efb22
umar <unzippinglinux@gmail.com>
2021-02-04 01:30:53 +0600

    completed the js

--- script.js
+++ script.js
@@ -16,7 +16,28 @@ function inputValueChanger(classType, changeType){
         currentNewCount = currentCount -  1;
     } 
     document.getElementById(classType + '-input').value = currentNewCount;
+    
+    //updating prices
+    updatePrices();
 }
+
+
+// price updater function 
+function updatePrices(){
+    let countFirstClass = document.getElementById('firstClass-input').value
+    let countEconomy = document.getElementById('economy-input').value
+    // firstClass unit ticket price = 150
+    // economy unit ticket price = 100
+    let subtotalPrice = parseInt(countFirstClass) * 150 + parseInt(countEconomy) * 100
+    let vat = subtotalPrice * 0.1
+    // showing subtotal
+    document.getElementById('subtotalValue').innerText  = subtotalPrice;
+    //showing vat
+    document.getElementById('vatValue').innerText  = vat; //vat is 10%
+    //showing total price
+    document.getElementById('totalValue').innerText = subtotalPrice + vat; 
+}
+
 
 // firstClass increasing values. 
 document.getElementById('firstClass-increment').addEventListener('click',()=>{
@@ -38,63 +59,11 @@ document.getElementById('economy-decrement').addEventListener('click',()=>{
     inputValueChanger('economy','decrement')
 })
 
+//alert showing the summery of the purchase
+// booking-btn
 
-// inputValueChanger('firstClass', 'decrement')
-// inputValueChanger('economy', 'increment')
-// inputValueChanger('economy', 'decrement')
-
-// function inputValueChanger(classType, changeType){
-//     if (changeType === 'increment'){
-//         document.getElementById(classType +'-increment').addEventListener('click',()=>{
-//             const inputValue = document.getElementById(classType + '-input').value;
-//             let currentCount = parseInt(inputValue);
-//                 currentCount +=  1;
-//             document.getElementById(classType + '-input').value = currentCount;
-//         })
-//     } else if(changeType === 'decrement'){
-//         document.getElementById(classType +'-decrement').addEventListener('click',()=>{
-//             const inputValue = document.getElementById(classType + '-input').value;
-//             let currentCount = parseInt(inputValue);
-//                 currentCount -=  1;
-//             document.getElementById(classType + '-input').value = currentCount;
-//         })
-//     }
-// }
-
-// inputValueChanger('firstClass', 'increment')
-// inputValueChanger('firstClass', 'decrement')
-// inputValueChanger('economy', 'increment')
-// inputValueChanger('economy', 'decrement')
-// function increment(classType){
-//     document.getElementById(classType +'-increment').addEventListener('click',()=>{
-//         const inputValue = document.getElementById(classType + '-input').value;
-//         let currentCount = parseInt(inputValue);
-//             currentCount +=  1;
-//         document.getElementById(classType + '-input').value = currentCount;
-//     })
-// }
-
-// function decrement(classType){
-//     document.getElementById(classType +'-decrement').addEventListener('click',()=>{
-//         const inputValue = document.getElementById(classType + '-input').value;
-//         let currentCount = parseInt(inputValue);
-//             currentCount -=  1;
-//         document.getElementById(classType + '-input').value = currentCount;
-//     })
-// }
-
-// increment('firstClass')
-// increment('economy')
-
-// decrement('firstClass')
-// decrement('economy')
-
-
-
-// document.getElementById('economy-decrement').addEventListener('click',()=>{
-//     const inputValue = document.getElementById('economy-input').value;
-//     let currentCount = parseInt(inputValue);
-//         currentCount -=  1;
-//     document.getElementById('economy-input').value = currentCount;
-// })
-
+document.getElementById('booking-btn').addEventListener('click', ()=>{
+    const firstClassTicket = document.getElementById('firstClass-input').value;
+    const economyTickets = document.getElementById('economy-input').value;
+    alert('You have purchased ' + firstClassTicket + ' first class ticket(s) and ' + economyTickets + ' economy ticket(s).');
+})
