b72e8fee857281b0ec3b96bb832119c76bfe2378
umar <unzippinglinux@gmail.com>
2021-02-03 23:21:21 +0600

    refactor js

--- script.js
+++ script.js
@@ -1,34 +1,70 @@
-// let currentValueFirstClass = 0;
-// let currentValueEconomy = 0;
-// base values of the input fields 
+// base values of the input fields. 
 document.getElementById('firstClass-input').value = 0;
 document.getElementById('economy-input').value = 0;
 
 
-// input value changer
+// input value changer function
 function inputValueChanger(classType, changeType){
+    const inputValue = document.getElementById(classType + '-input').value;
+    let currentCount = parseInt(inputValue);
+    let currentNewCount = currentCount;
     if (changeType === 'increment'){
-        document.getElementById(classType +'-increment').addEventListener('click',()=>{
-            const inputValue = document.getElementById(classType + '-input').value;
-            let currentCount = parseInt(inputValue);
-                currentCount +=  1;
-            document.getElementById(classType + '-input').value = currentCount;
-        })
-    } else if(changeType === 'decrement'){
-        document.getElementById(classType +'-decrement').addEventListener('click',()=>{
-            const inputValue = document.getElementById(classType + '-input').value;
-            let currentCount = parseInt(inputValue);
-                currentCount -=  1;
-            document.getElementById(classType + '-input').value = currentCount;
-        })
+        currentNewCount = currentCount +  1;
     }
+    if (changeType === 'decrement' &&
+        currentCount>0){
+        currentNewCount = currentCount -  1;
+    } 
+    document.getElementById(classType + '-input').value = currentNewCount;
 }
 
-inputValueChanger('firstClass', 'increment')
-inputValueChanger('firstClass', 'decrement')
-inputValueChanger('economy', 'increment')
-inputValueChanger('economy', 'decrement')
+// firstClass increasing values. 
+document.getElementById('firstClass-increment').addEventListener('click',()=>{
+    inputValueChanger('firstClass','increment')
+})
 
+//firstClass decreasing values.
+document.getElementById('firstClass-decrement').addEventListener('click',()=>{
+    inputValueChanger('firstClass','decrement')
+})
+
+//economy increasing values.
+document.getElementById('economy-increment').addEventListener('click',()=>{
+    inputValueChanger('economy','increment')
+})
+
+//economy decreasing values.
+document.getElementById('economy-decrement').addEventListener('click',()=>{
+    inputValueChanger('economy','decrement')
+})
+
+
+// inputValueChanger('firstClass', 'decrement')
+// inputValueChanger('economy', 'increment')
+// inputValueChanger('economy', 'decrement')
+
+// function inputValueChanger(classType, changeType){
+//     if (changeType === 'increment'){
+//         document.getElementById(classType +'-increment').addEventListener('click',()=>{
+//             const inputValue = document.getElementById(classType + '-input').value;
+//             let currentCount = parseInt(inputValue);
+//                 currentCount +=  1;
+//             document.getElementById(classType + '-input').value = currentCount;
+//         })
+//     } else if(changeType === 'decrement'){
+//         document.getElementById(classType +'-decrement').addEventListener('click',()=>{
+//             const inputValue = document.getElementById(classType + '-input').value;
+//             let currentCount = parseInt(inputValue);
+//                 currentCount -=  1;
+//             document.getElementById(classType + '-input').value = currentCount;
+//         })
+//     }
+// }
+
+// inputValueChanger('firstClass', 'increment')
+// inputValueChanger('firstClass', 'decrement')
+// inputValueChanger('economy', 'increment')
+// inputValueChanger('economy', 'decrement')
 // function increment(classType){
 //     document.getElementById(classType +'-increment').addEventListener('click',()=>{
 //         const inputValue = document.getElementById(classType + '-input').value;
@@ -61,34 +97,4 @@ inputValueChanger('economy', 'decrement')
 //         currentCount -=  1;
 //     document.getElementById('economy-input').value = currentCount;
 // })
-
-
-
-
-// document.getElementById('economy-increment').addEventListener('click',()=>{
-//     const inputValue = document.getElementById('economy-input').value;
-//     let currentCount = parseInt(inputValue);
-//         currentCount +=  1;
-//     document.getElementById('economy-input').value = currentCount;
-// })
-// document.getElementById('economy-decrement').addEventListener('click',()=>{
-//     const inputValue = document.getElementById('economy-input').value;
-//     let currentCount = parseInt(inputValue);
-//         currentCount -=  1;
-//     document.getElementById('economy-input').value = currentCount;
-// })
-
-// document.getElementById('firstClass-increment').addEventListener('click',()=>{
-//     const inputValue = document.getElementById('firstClass-input').value;
-//     let currentCount = parseInt(inputValue);
-//         currentCount +=  1;
-//     document.getElementById('firstClass-input').value = currentCount;
-// })
-// document.getElementById('firstClass-decrement').addEventListener('click',()=>{
-//     const inputValue = document.getElementById('firstClass-input').value;
-//     let currentCount = parseInt(inputValue);
-//         currentCount -=  1;
-//     document.getElementById('firstClass-input').value = currentCount;
-// })
-
 
